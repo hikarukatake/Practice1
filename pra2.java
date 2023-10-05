dd53f476a046a6f345b189f15100db6d827ae363
hikaru <h.matsuba.sys23@morijyobi.ac.jp>
2023-10-05 10:38:48 +0900

    Hello World を １００かい出力する

--- pra2.java
+++ pra2.java
@@ -2,8 +2,8 @@ public  class Hello
 public static void main(String args[]){
 	int sum = 0;
 	while(true){
-		sum += 1; 
-		System.out.println(Hello World);
+		sum++; 
+		System.out.println("Hello World");
 		if(sum > 100){
 			break;
 		}
