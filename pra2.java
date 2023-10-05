7934950d49258e8d293b32e475dc9e30aed45c65
hikaru <h.matsuba.sys23@morijyobi.ac.jp>
2023-10-05 10:18:40 +0900

    Hello World を １００回記述する

--- pra2.java
+++ pra2.java
@@ -1,1 +1,11 @@
-Hello World
+public  class Hello
+public static void main(String args[]){
+	int sum = 0;
+	while(true){
+		sum += 1; 
+		System.out.println(Hello World);
+		if(sum > 100){
+			break;
+		}
+	}
+}
