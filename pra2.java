9c19e38031f951c2e764e1a2cc58696a8de8b7a6
hikaru <h.matsuba.sys23@morijyobi.ac.jp>
2023-10-24 12:09:18 +0900

    課題の修正
    while文をfor文に変更

--- pra2.java
+++ pra2.java
@@ -1,11 +1,8 @@
-public  class Hello
+public  class Hello{
 public static void main(String args[]){
 	int sum = 0;
-	while(true){
-		sum++; 
-		System.out.println("Hello World");
-		if(sum > 100){
-			break;
+	for(int i = 0; i < 100;i++){
+		System.out.println("Hello World")
 		}
 	}
 }
